fc646c4490946148c8dd2b02563c24893100da19
Stefan Djokic <stefan.djol3@gmail.com>
2022-07-18 02:48:26 +0200

    Add getAirports script

--- service/getAirports.ts
+++ service/getAirports.ts
@@ -4,10 +4,9 @@ require('dotenv').config();
 import { Airport, mergeAirports } from 'clients';
 import { RyanAirClient } from 'clients/ryanair';
 import { WizzAirClient } from 'clients/wizzair';
-import { saveAirports, saveFares } from 'helpers/db';
+import { saveAirports } from 'helpers/db';
 
 async function run () {
-  const airportCodes: string[] = process.env.AIRPORTS?.split(',') || [];
   const lookupDays = parseInt(process.env.LOOKUP_DAYS || '30');
 
   const ryanAirClient = new RyanAirClient({ lookupDays });
@@ -19,18 +18,9 @@ async function run () {
   ]);
 
   const airports: Airport[] = mergeAirports([ryanAirClient.airports, wizzAirClient.airports]);
-  const filteredAirports = airports.filter(({ code }) => airportCodes.includes(code));
 
   await saveAirports(airports);
   console.log('Saved Airports to DB!');
-
-  await Promise.all([
-    ryanAirClient.getFares(filteredAirports),
-    wizzAirClient.getFares(filteredAirports)
-  ]);
-
-  await saveFares([...ryanAirClient.fares, ...wizzAirClient.fares]);
-  console.log('Saved Fares to DB!');
 }
 
 run();
