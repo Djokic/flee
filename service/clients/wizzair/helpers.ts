d07bb1d7fa5fd1998308903b63f79511d6839fea
Stefan Djokic <stefan.djol3@gmail.com>
2023-07-28 16:26:21 +0200

    Update workflows

--- service/clients/wizzair/helpers.ts
+++ service/clients/wizzair/helpers.ts
@@ -1,5 +1,8 @@
-
 export async function getApiUrl (): Promise<string> {
+  if (process.env.WIZZAIR_API_URL) {
+    return process.env.WIZZAIR_API_URL;
+  }
+
   const request = await fetch('https://wizzair.com/static_fe/metadata.json');
   const data = await request.json();
   return data.apiUrl;
