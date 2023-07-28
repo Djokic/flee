57fd2812cd64071aa4f6dc85a0264cca5cc3689b
Stefan Djokic <stefan.djol3@gmail.com>
2023-07-28 18:37:32 +0200

    Additional cleanup of service

--- service/helpers/dbDriver.ts
+++ service/helpers/dbDriver.ts
@@ -4,7 +4,7 @@ if (!process.env.DB_URL || !process.env.DB_USER || !process.env.DB_PASSWORD) {
   throw new Error('Missing DB_URL, DB_USER or DB_PASSWORD environment variable');
 }
 
-export const driver = neo4j.driver(
+export const dbDriver = neo4j.driver(
   process.env.DB_URL,
   neo4j.auth.basic(process.env.DB_USER, process.env.DB_PASSWORD)
 );
